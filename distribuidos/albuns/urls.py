79e5a2fb0ae183324e990f511b46f2a99fae8f4a
evandrolrff <evandrolrff@gmail.com>
2017-10-16 10:43:03 -0200

    Inserção de músicas e albuns feito

--- distribuidos/albuns/urls.py
+++ distribuidos/albuns/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     url(r'^$', views.page_index, name='page_index'),
     url(r'^albuns/$', views.list_albuns, name='list_albuns'), 
     url(r'^musicas/$', views.list_musicas, name='list_musicas'),
+    url(r'^albuns/new/$', views.albuns_new, name='albuns_new'),
+    url(r'^musicas/new/$', views.musicas_new, name='musicas_new'),
+    url(r'^albuns_add/$', views.albuns_add, name='albuns_add'),
+    url(r'^musicas_add/$', views.musicas_add, name='musicas_add'),
 ]
--- distribuidos/albuns/views.py
+++ distribuidos/albuns/views.py
@@ -8,15 +8,42 @@ def page_index(request):
 
 def list_albuns(request):
     albuns = Album.objects.all()
-    context = {
-        'albuns' : albuns,
-    }
-    return render(request, 'albuns/list_albuns.html', context)
+    return render(request, 'albuns/list_albuns.html', {'albuns': albuns})
 
 def list_musicas(request):
     musicas = Musica.objects.all()
-    context = {
-        'musicas': musicas,
-    }
+    return render(request, 'albuns/list_musicas.html', {'musicas': musicas})
 
-    return render(request, 'albuns/list_musicas.html', context)
+def albuns_new(request):
+    return render(request, 'albuns/albuns_edit.html', {})
+
+def musicas_new(request):
+    albuns = Album.objects.all()
+    return render(request, 'albuns/musicas_edit.html', {'albuns': albuns})
+
+def albuns_add(request):
+    nome_form = request.POST.get("nome")
+    data_lancamento_form = request.POST.get("data_lancamento")
+    banda_form = request.POST.get("banda")
+
+    album = Album(
+        nome = nome_form,
+        data_lancamento = data_lancamento_form,
+        banda = banda_form
+    )
+    album.save()
+    return redirect('/albuns/')
+
+def musicas_add(request):
+    album_form = Album.objects.get(nome=request.POST.get("album"))
+    print(album_form)
+    nome_form = request.POST.get("nome")
+    duracao_form = request.POST.get("duracao")
+
+    musica = Musica(
+        album = album_form,
+        nome = nome_form,
+        duracao = duracao_form
+    )
+    musica.save()
+    return redirect('/musicas/')
